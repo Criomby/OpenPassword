91955a81a6cdd0207a5ed8810bf69460be6ead1c
Philippe <philippebraum@pm.me>
2022-10-28 09:50:01 -0400

    closing issue #7

--- static/js/site.js
+++ static/js/site.js
@@ -1,13 +1,13 @@
 // add footer content into the footer element
 const footer = document.getElementById('footer');
 footer.innerHTML = `
-  <div class="footer-left"><i class="fa-solid fa-rectangle-xmark"></i> No cookies, trackers, ads<br>
-    <i class="fa-solid fa-person"></i> Right to freedom<br>
-  </div>
-  <div class="footer-right">
-    <i class="fa-regular fa-circle-user"></i> By <a href="https://philippebraum.com/" class="hover-underline-animation" style="color: blue">Philippe Braum</a><br>
-    ${siteVersion}
-  </div>
+<div class="footer-left">No trackers or ads<br>
+  Right to freedom<br>
+</div>
+<div class="footer-right">
+  By Philippe Braum<br>
+  v${siteVersion}
+</div>
 `;
 
 // buttons
